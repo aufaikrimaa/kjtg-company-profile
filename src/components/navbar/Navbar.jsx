6f7928efd06d79317ed7d992e4bf447f96e34f17
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-03-19 19:20:34 +0700

    styling navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo-dialz.png";
 import "./navbar.css";
 
 function Navbar() {
+  const navbarRef = useRef(null);
+  useEffect(() => {
+    const shrinkNavbar = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        navbarRef.current.classList.replace("navbar", "shrink");
+      } else {
+        navbarRef.current.classList.replace("shrink", "navbar");
+      }
+    };
+    window.addEventListener("scroll", shrinkNavbar);
+    return () => {
+      window.removeEventListener("scroll", shrinkNavbar);
+    };
+  }, []);
   return (
     <>
-      <div className="navbar fixed z-10">
-        <div className="container flex px-5 py-2 justify-between font-bold text-white">
+      <div ref={navbarRef} className="navbar fixed z-10">
+        <div className="container flex px-6 py-2 justify-between font-bold text-white">
           <div className="logo">
             <div className="flex ">
               <img src={logo} alt="" className="w-8 h-auto " />
@@ -17,12 +35,12 @@ function Navbar() {
           </div>
           <div className="nav self-center">
             <Link to="/" className="mr-4">
-              Home
+              Beranda
             </Link>
             <Link to="/" className="mr-4">
               Paket Wisata
             </Link>
-            <Link to="/">Gallery</Link>
+            <Link to="/">Galeri</Link>
           </div>
         </div>
       </div>
