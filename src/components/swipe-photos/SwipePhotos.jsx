48b119692d1f1a4f293ab6793ae18c84e738fb09
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-03-20 09:10:32 +0700

    add swipe photos

--- src/components/swipe-photos/SwipePhotos.jsx
+++ src/components/swipe-photos/SwipePhotos.jsx
@@ -1,10 +1,51 @@
+import imgUrl from "../data/photoData";
+import { Autoplay, EffectCoverflow } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "./swipe-photos.css";
+
 function SwipePhotos() {
-  const imageUrl =
-    "https://lh3.googleusercontent.com/drive-viewer/AKGpihbDy3sbzyZDw3j2vYaAcVNMQfpo_LtRHLZeSLwVXmHD8MuSoHszNzk58_Z4cRCNPzQRxIi9tuP3yuw3gRSzB71fkXz9=s1600";
   return (
     <>
-      <div>
-        <img src={imageUrl} alt="testphotogdrive" />
+      <div className="photos flex">
+        <Swiper
+          modules={[Autoplay, EffectCoverflow]}
+          autoplay={{ delay: 3000 }}
+          speed={1000}
+          loop={true}
+          grabCursor={true}
+          slidesPerView={5}
+          delay={200}
+          effect="coverflow"
+          coverflowEffect={{
+            rotate: 0,
+            stretch: -60,
+            scale: 0.9,
+            slideShadows: false,
+          }}
+          className="swiper-photo self-center"
+          onSlideChange={(swiper) => {
+            const slides = swiper.slides;
+            slides.forEach((slide, index) => {
+              const img = slide.querySelector("img");
+              if (index === swiper.activeIndex + 2) {
+                img.style.opacity = 1;
+              } else {
+                img.style.opacity = 0.5;
+              }
+            });
+          }}
+        >
+          {imgUrl.map((img, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={img}
+                alt={`image ${index}`}
+                className="img-swipe h-24 rounded-md opacity-transition"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </>
   );
