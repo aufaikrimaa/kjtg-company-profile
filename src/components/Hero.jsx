de5c2b2865f0acd1d7fa86b730af86bb6d43cd8b
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-03-20 17:32:37 +0700

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,10 @@ function Hero() {
           <div className="px-16">
             <div>
               <div className="text-5xl font-bold mb-4">
-                Komunitas <span className="text-[#e1d29b]">Jeep</span>
+                Komunitas <span className="text-[#FFD278]">Jeep</span>
               </div>
               <div className="text-6xl font-bold mb-4">
-                <span className="text-[#e1d29b]">Tour</span> Galunggung
+                <span className="text-[#FFD278]">Tour</span> Galunggung
               </div>
               <div className="text-2xl font-medium leading-9">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do
