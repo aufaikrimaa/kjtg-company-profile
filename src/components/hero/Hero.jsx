de6cffa3831f86acc8d592b63be2ede7880bd52d
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-03-20 11:45:41 +0700

    add content hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,22 @@ import "./hero.css";
 function Hero() {
   return (
     <>
-      <div className="hero relative"></div>
+      <div className="hero relative flex">
+        <div className="container z-1 text-white self-center">
+          <div className="px-16">
+            <div>
+              <div className="text-6xl font-bold mb-4">
+                Dialz <span className="text-[#e1d29b]">Tour</span>
+              </div>
+              <div className="text-2xl font-medium leading-9">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do
+                <br />
+                eiusmod tempor incididunt ut labore et dolore
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
