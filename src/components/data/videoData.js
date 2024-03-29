5cb74fbb93a947ed056ae116c7533c68ef6e9830
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-03-29 12:28:49 +0700

    add data video

--- src/components/data/videoData.js
+++ src/components/data/videoData.js
@@ -1,12 +1,25 @@
 import cover1 from "../../assets/images/videos-cover/1.png";
 import cover2 from "../../assets/images/videos-cover/2.png";
 import cover3 from "../../assets/images/videos-cover/3.png";
+import cover4 from "../../assets/images/videos-cover/4.png";
+import cover5 from "../../assets/images/videos-cover/5.png";
+import cover6 from "../../assets/images/videos-cover/6.png";
+import cover7 from "../../assets/images/videos-cover/7.png";
+import cover8 from "../../assets/images/videos-cover/8.png";
+import cover9 from "../../assets/images/videos-cover/9.png";
+import cover10 from "../../assets/images/videos-cover/10.png";
+import cover11 from "../../assets/images/videos-cover/11.png";
+import cover12 from "../../assets/images/videos-cover/12.png";
+import cover13 from "../../assets/images/videos-cover/13.png";
+import cover14 from "../../assets/images/videos-cover/14.png";
+import cover15 from "../../assets/images/videos-cover/15.png";
+import cover16 from "../../assets/images/videos-cover/16.png";
+import cover17 from "../../assets/images/videos-cover/17.png";
+import cover18 from "../../assets/images/videos-cover/18.png";
+import cover19 from "../../assets/images/videos-cover/19.png";
+import cover20 from "../../assets/images/videos-cover/20.png";
 
 const video = [
-  {
-    cover: cover1,
-    link: "https://www.instagram.com/reel/C2GbZdkB1Dr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
-  },
   {
     cover: cover2,
     link: "https://www.instagram.com/reel/C5A0o_kLS7x/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
@@ -14,6 +27,79 @@ const video = [
   {
     cover: cover3,
     link: "https://www.instagram.com/reel/C4xcm1LrDtd/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover9,
+    link: "https://www.instagram.com/reel/C3_oEDaB36P/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover12,
+    link: "https://www.instagram.com/reel/C3ogTJbv4pm/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover13,
+    link: "https://www.instagram.com/reel/C3nELj1v2vS/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover14,
+    link: "https://www.instagram.com/reel/C3mBCZTLSM8/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover15,
+    link: "https://www.instagram.com/reel/C3hYPZNrvMf/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover20,
+    link: "https://www.instagram.com/reel/C2l8cM1LkOb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover11,
+    link: "https://www.instagram.com/reel/C31mBj-PIMm/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover4,
+    link: "https://www.instagram.com/reel/C4W0YI-puu5/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover5,
+    link: "https://www.instagram.com/reel/C4JsEPuBzIG/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover6,
+    link: "https://www.instagram.com/reel/C4ISl0yrZcC/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover7,
+    link: "https://www.instagram.com/reel/C4IM-tAr8lj/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover8,
+    link: "https://www.instagram.com/reel/C4GGocwyePB/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+
+  {
+    cover: cover10,
+    link: "https://www.instagram.com/reel/C392HPVryN5/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover1,
+    link: "https://www.instagram.com/reel/C2GbZdkB1Dr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover16,
+    link: "https://www.instagram.com/reel/C3UbkQGrZRc/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover17,
+    link: "https://www.instagram.com/reel/C3KGPQhrg4_/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover18,
+    link: "https://www.instagram.com/reel/C21cZhzrU7L/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    cover: cover19,
+    link: "https://www.instagram.com/reel/C2xJE46yWsz/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
   },
 ];
 
