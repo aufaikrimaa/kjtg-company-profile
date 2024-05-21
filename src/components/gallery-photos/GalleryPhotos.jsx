d0cedcb22a1312c73be4989bb77498b4feead81e
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-05-21 13:16:57 +0700

    updates

--- src/components/gallery-photos/GalleryPhotos.jsx
+++ src/components/gallery-photos/GalleryPhotos.jsx
@@ -12,22 +12,9 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
 function GalleryPhotos() {
   const [loaded, setLoaded] = useState(false);
   const [photos, setPhotos] = useState([]);
-
-  const myElementRefs = useRef([]);
 
   useEffect(() => {
     loadPhotos()
@@ -40,42 +27,11 @@ function GalleryPhotos() {
       });
   }, []);
 
-  useEffect(() => {
-    // Menambahkan event listener untuk scroll
-    window.addEventListener("scroll", showElementsOnScroll);
-
-    // Menjalankan fungsi saat komponen dimuat
-    showElementsOnScroll();
-
-    // Membersihkan event listener saat komponen di-unmount
-    return () => {
-      window.removeEventListener("scroll", showElementsOnScroll);
-    };
-  }, []);
-
-  // Fungsi untuk menambah kelas 'show' jika elemen berada di viewport
-  const showElementsOnScroll = () => {
-    myElementRefs.current.forEach((el) => {
-      if (isElementInViewport(el)) {
-        el.classList.add("show");
-      }
-    });
-  };
-  // Mengambil referensi untuk setiap elemen dengan kelas 'myElement'
-  const setRefs = (el) => {
-    if (el && !myElementRefs.current.includes(el)) {
-      myElementRefs.current.push(el);
-    }
-  };
-
   return (
     <>
       <div className="gallery-photos text-white">
         <div className="mx-[2rem] sm:mx-0">
-          <div
-            ref={setRefs}
-            className="myElement gphotos-title text-2xl sm:text-lg flex justify-center font-bold mb-4 pb-1 relative"
-          >
+          <div className="gphotos-title text-2xl sm:text-lg flex justify-center font-bold mb-4 pb-1 relative">
             Galeri Foto
           </div>
           <div className="flex flex-wrap justify-center pb-10">
